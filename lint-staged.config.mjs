1722d0e11b78fc2039adb9b6c55f53cd06fdd40f
petarzarkov <pzarko1@gmail.com>
2025-11-01 12:44:07 +0200

    fix: commit msg for precommit hook

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,7 +2,7 @@
  * @type {import('lint-staged')}
  */
 const config = {
-  '*.{ts}': ['pnpm lint', 'pnpm format'],
+  '*.ts': ['pnpm lint', 'pnpm format'],
   '*.{json,css,scss,md}': ['pnpm format'],
   '.env.sample': ['pnpm gen:env:docs && git add env-vars.md'],
 };
